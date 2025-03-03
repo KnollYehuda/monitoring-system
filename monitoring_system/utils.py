01d59acd017df0d4cdfd06623e6cc962cf493b54
ibex-yehuda <yehuda.knoll@ibex-ai.com>
2025-03-03 18:35:45 +0200

    Add some docstring

--- monitoring_system/utils.py
+++ monitoring_system/utils.py
@@ -3,4 +3,10 @@ from string import ascii_lowercase
 
 
 def string_generator(length: int = 50):
+    """Generate a random string of lowercase letters.
+
+    :param length: The length of the generated string. Defaults to 50.
+    :return randomly generated string of lowercase letters.
+    """
+
     return "".join(choice(ascii_lowercase) for _ in range(length))
